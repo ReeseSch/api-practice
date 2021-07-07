d606d7772f34559ed834fc297409df01c6a8a330
Reese Schumacher <85515460+ReeseSch@users.noreply.github.com>
2021-07-07 14:05:31 -0600

    upload1

---
+++ main.js
@@ -0,0 +1,29 @@
+// const { default: axios } = require("axios")
+
+// const { default: axios } = require("axios")
+
+const button = document.querySelector('button')
+const body = document.querySelector('div')
+const errorCallBack = err => console.log(err)
+// const app = express();
+// app.use(express.json())
+// app.use(cors())
+
+const clicked = () => {
+    console.log(`You clicked a button! You must be so proud`)
+    axios.get('https://swapi.dev/api/planets/?search=alderaan').then((res) => {
+        console.log(res.data)
+        let residents = res.data.results[0].residents
+        for( let i = 0; i <= residents.length; i++ ) {
+            axios.get(residents[i]).then((res) => {
+                console.log(res.data.name)
+                const nameDisplay = document.createElement('h2')
+                let name = res.data.name
+            nameDisplay.textContent = `${name} lives on Alderaan! Good for them!`
+            body.append(nameDisplay)
+        }).catch(errorCallBack)
+    }}).catch(errorCallBack)
+}   
+
+
+button.addEventListener('click', clicked)
